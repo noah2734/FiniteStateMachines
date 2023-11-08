9d039011e006df43a5c1cc15145f0cc5f32f512c
Noah Robert Mcclelland <noahmcclelland1738@gmail.com>
2023-11-08 12:53:59 -0700

    added compatability with vscode

---
+++ FiniteStateMachinesApp/build/CMakeFiles/FiniteStateMachinesApp.dir/cmake_clean.cmake
@@ -0,0 +1,22 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/FiniteStateMachinesApp_autogen.dir/AutogenUsed.txt"
+  "CMakeFiles/FiniteStateMachinesApp_autogen.dir/ParseCache.txt"
+  "FiniteStateMachinesApp_autogen"
+  "CMakeFiles/FiniteStateMachinesApp.dir/FiniteStateMachinesApp_autogen/mocs_compilation.cpp.o"
+  "CMakeFiles/FiniteStateMachinesApp.dir/FiniteStateMachinesApp_autogen/mocs_compilation.cpp.o.d"
+  "CMakeFiles/FiniteStateMachinesApp.dir/dfadialog.cpp.o"
+  "CMakeFiles/FiniteStateMachinesApp.dir/dfadialog.cpp.o.d"
+  "CMakeFiles/FiniteStateMachinesApp.dir/machine.cpp.o"
+  "CMakeFiles/FiniteStateMachinesApp.dir/machine.cpp.o.d"
+  "CMakeFiles/FiniteStateMachinesApp.dir/main.cpp.o"
+  "CMakeFiles/FiniteStateMachinesApp.dir/main.cpp.o.d"
+  "CMakeFiles/FiniteStateMachinesApp.dir/mainwindow.cpp.o"
+  "CMakeFiles/FiniteStateMachinesApp.dir/mainwindow.cpp.o.d"
+  "FiniteStateMachinesApp.app/Contents/MacOS/FiniteStateMachinesApp"
+  "FiniteStateMachinesApp.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/FiniteStateMachinesApp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ FiniteStateMachinesApp/build/CMakeFiles/FiniteStateMachinesApp_autogen.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/FiniteStateMachinesApp_autogen"
+  "FiniteStateMachinesApp_autogen/include/ui_dfadialog.h"
+  "FiniteStateMachinesApp_autogen/include/ui_mainwindow.h"
+  "FiniteStateMachinesApp_autogen/mocs_compilation.cpp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/FiniteStateMachinesApp_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
